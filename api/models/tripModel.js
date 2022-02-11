ee09dcc664d3c30ef1652f6088bca928973e4643
RocVecino <94741402+RocVecino@users.noreply.github.com>
2022-02-11 18:03:22 +0100

    Update tripModel.js

--- api/models/tripModel.js
+++ api/models/tripModel.js
@@ -1,65 +1,63 @@
-'use strict'
-const mongoose = require('mongoose') 
-const Schema = mongoose.Schema
+  'use strict'
+  const mongoose = require('mongoose') 
+  const Schema = mongoose.Schema
 
-const StageSchema = new Schema({
-    title: {
+  const StageSchema = new Schema({
+      title: {
+        type: String,
+        required: 'Kindly enter the title of the stage'
+      },
+      description: {
+        type: String
+      },
+      price: {
+        type: Double
+      }
+    }, { strict: false })
+
+  const TripSchema = new Schema({
+    ticker: {
       type: String,
-      required: 'Kindly enter the title of the comment'
+      required: 'Kindly enter the ticker of the trip'
     },
-    description: {
-      type: String
+    title: { 
+      type: String, 
+      required: 'Kindly enter the title of the trip'
+    },
+    description: { 
+      type: String,
+      required: 'Kindly enter the title of the trip'
     },
     price: {
-      type: Double,
-      // TODO: digitos
-    }
+      type: Double
+    },
+    requirements: {
+        type:[String]
+    },
+    start_date: {
+        type: Date
+    },
+    end_date: {
+        type: Date
+    },
+    pictures: {
+        type: [{data: Buffer, contentType: String}]
+    },
+    canceled: {
+      type: Boolean
+    },
+    cancelReason: {
+        type: String
+    },
+    manager_Id: {
+      type: Schema.Types.ObjectId,
+      required: 'manager id required'
+    },
+    application_id: {
+      type: Schema.Types.ObjectId,
+      required: 'application id required'
+    },
+    stages : [StageSchema]
   }, { strict: false })
 
-const TripSchema = new Schema({
-  ticker: {
-    type: String,
-    required: 'Kindly enter the actor name'
-  },
-  title: { 
-    type: String, 
-    required: 'Kindly enter the actor surname'
-  },
-  description: { 
-    type: String,
-    required: 'Kindly enter the actor surname'
-  },
-  price: {
-    type: Double
-    //TODO: digitos
-  },
-  requirements: {
-      type:[String]
-  },
-  start_date: {
-      type: Date
-  },
-  end_date: {
-      type: Date
-  },
-  pictures: {
-      type: [{data: Buffer, contentType: String}]
-  },
-  canceled: {
-    type: Boolean
-  },
-  cancelReason: {
-      type: String
-  },
-  manager_Id: {
-    type: Schema.Types.ObjectId,
-    required: 'consumer id required'
-  },
-  application_id: {
-    type: Schema.Types.ObjectId,
-    required: 'consumer id required'
-  },
-  stages : [StageSchema]
-}, { strict: false })
-
-module.exports = mongoose.model('Trips', TripSchema)
+  module.exports = mongoose.model('Trips', TripSchema)
