b30b9b38aba810846c2908afa5402bbf83d76c2c
lucaspreel <preel.lucas@gmail.com>
2022-02-13 22:50:57 +0100

    add basic sponsorship endpoints

--- api/controllers/sponsorshipController.js
+++ api/controllers/sponsorshipController.js
@@ -4,26 +4,58 @@ const mongoose = require('mongoose')
 const Sponsorship = mongoose.model('Sponsorship')
 
 exports.create_a_sponsorship = function (req, res) {
-    console.log("Create a sponsorship")
+    const newSponsorship = new Sponsorship(req.body)
+
+    newSponsorship.save(function (err, sponsorship) {
+      if (err) {
+        res.send(err)
+      } else {
+        res.json(sponsorship)
+      }
+    })
 }
 
 exports.list_all_sponsorships = function (req, res) {
-    console.log("List all sponsorships")
+    Sponsorship.find({}, function (err, sponsorships) {
+        if (err) {
+          res.send(err)
+        } else {
+          res.json(sponsorships)
+        }
+      })
 }
 
 exports.read_a_sponsorship = function (req, res) {
     // must only be possible if the sponsorship was paid
-    console.log("Read a sponsorship")
+    Sponsorship.findById(req.params.sponsorshipId, function (err, sponsorship) {
+        if (err) {
+          res.send(err)
+        } else {
+          res.json(sponsorship)
+        }
+      })
 }
 
 exports.update_a_sponsorship = function (req, res) {
     // only the sponsor concerned by this sponsorship must be able to do that
-    console.log("Update a sponsorship")
+    Sponsorship.findOneAndUpdate({ _id: req.params.sponsorshipId }, req.body, { new: true }, function (err, sponsorship) {
+        if (err) {
+            res.send(err)
+        } else {
+            res.json(sponsorship)
+        }
+    })
 }
 
 exports.delete_a_sponsorship = function (req, res) {
     // only the sponsor concerned by this sponsorship must be able to do that
-    console.log("Delete a sponsorship")
+    Sponsorship.deleteOne({ _id: req.params.sponsorshipId }, function (err, sponsorship) {
+        if (err) {
+          res.send(err)
+        } else {
+          res.json({ message: 'Sponsorship successfully deleted' })
+        }
+      })
 }
 
 exports.pay_a_sponsorship = function (req, res) {
