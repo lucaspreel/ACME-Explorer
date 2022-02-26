0a6020692d211209c39c3a706ab00f12d374f9b7
Jessie Romero Perez <jessieromeroperez@gmail.com>
2022-02-27 00:39:25 +0100

    ADD: cambios de estados

--- api/routes/applicationRoutes.js
+++ api/routes/applicationRoutes.js
@@ -1,21 +1,37 @@
-'use strict';
+"use strict";
 module.exports = function (app) {
-  const application = require('../controllers/applicationController');
+  const application = require("../controllers/applicationController");
 
   app
-    .route('/v1/applications')
+    .route("/v1/applications")
     .get(application.list_all_application)
     .post(application.create_an_application);
 
   app
-    .route('/v1/applications/:applicationId')
+    .route("/v1/applications/:applicationId")
     .delete(application.delete_an_application);
 
   app
-    .route('/v1/applications/manager/:managerId')
+    .route("/v1/applications/manager/:managerId")
     .get(application.find_by_manager_id);
 
   app
-    .route('/v1/applications/explorer/:explorerId')
+    .route("/v1/applications/explorer/:explorerId")
     .get(application.find_by_explorer_id);
+
+  app
+    .route("/v1/applications/:applicationId/reject")
+    .patch(application.reject_application);
+
+  app
+    .route("/v1/applications/:applicationId/due")
+    .patch(application.due_application);
+
+  app
+    .route("/v1/applications/:applicationId/pay")
+    .patch(application.pay_application);
+
+  app
+    .route("/v1/applications/:applicationId/cancel")
+    .patch(application.cancelled_application);
 };
