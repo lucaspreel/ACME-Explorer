53b5f2700135ae24c7fa7dfd254445d3c0fa783c
Jessie Romero Perez <jessieromeroperez@gmail.com>
2022-02-28 16:00:24 +0100

    Fix: puertos en app,js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 18080;
-// const port = process.env.PORT || 8089;
+const port = process.env.PORT || 8089;
 const mongoose = require('mongoose');
 
 const Actor = require('./api/models/actorModel');
@@ -54,14 +53,12 @@ routesApplication(app);
 routesFinder(app);
 
 // MongoDB URI building
-// const mongoDBUser = process.env.mongoDBUser || 'ACME_EXPLORER_ADMIN_USER';
-// const mongoDBPass = process.env.mongoDBPass || '$3CUR3p455W0RDZOZZ';
-// const mongoDBCredentials = (mongoDBUser && mongoDBPass) ? mongoDBUser + ':' + mongoDBPass + '@' : '';
-const mongoDBCredentials = "";
+const mongoDBUser = process.env.mongoDBUser || 'ACME_EXPLORER_ADMIN_USER';
+const mongoDBPass = process.env.mongoDBPass || '$3CUR3p455W0RDZOZZ';
+const mongoDBCredentials = (mongoDBUser && mongoDBPass) ? mongoDBUser + ':' + mongoDBPass + '@' : '';
 
 const mongoDBHostname = process.env.mongoDBHostname || 'localhost';
-// const mongoDBPort = process.env.mongoDBPort || '27017';
-const mongoDBPort = process.env.mongoDBPort || '30017';
+const mongoDBPort = process.env.mongoDBPort || '27017';
 const mongoDBName = process.env.mongoDBName || 'ACME-Explorer';
 
 const mongoDBURI = 'mongodb://' + mongoDBCredentials + mongoDBHostname + ':' + mongoDBPort + '/' + mongoDBName;
